bc3dae568ebaf23a96c4f2b7e8b718a5cceeefe7
xianti-towify <xianti.xiong@towify.com>
2024-06-02 20:32:49 +0800

    fix(xiong): add semi-ui

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { semiTheming } from 'vite-plugin-semi-theming'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+      react(),
+      semiTheming({
+        theme: "@semi-bot/semi-theme-feishu-dashboard"
+      })
+  ],
+  server: {
+    host: "0.0.0.0",
+  },
 })
